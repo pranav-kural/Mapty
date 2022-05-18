27e1eb1175774eb2e9e4253677e23d128915997e
pranav-kural <pranavkural042@gmail.com>
2022-05-18 13:24:19 -0600

    Added leaflet and diplaying map

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  pos => {
+    const { latitude } = pos.coords;
+    const { longitude } = pos.coords;
+
+    const map = L.map('map').setView([latitude, longitude], 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  () => alert('Could not get your position')
+);
