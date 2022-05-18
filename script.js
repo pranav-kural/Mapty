52d51b805e4634f1de262288512edb75a26eef7a
pranav-kural <pranavkural042@gmail.com>
2022-05-18 16:43:24 -0600

    render popup on click on map

--- script.js
+++ script.js
@@ -15,18 +15,42 @@ navigator.geolocation.getCurrentPosition(
   pos => {
     const { latitude } = pos.coords;
     const { longitude } = pos.coords;
-
-    const map = L.map('map').setView([latitude, longitude], 16);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    // render leaflet interactive map
+    leafletMap(latitude, longitude);
   },
   () => alert('Could not get your position')
 );
+
+const leafletMap = function (latitude, longitude) {
+  // render map
+  const map = renderMap(latitude, longitude);
+
+  // map click event handler
+  map.on('click', mapEvent => mapClickHandler(mapEvent, map));
+};
+
+const renderMap = (latitude, longitude) => {
+  const map = L.map('map').setView([latitude, longitude], 16);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  return map;
+};
+
+const mapClickHandler = (mapEvent, map) => {
+  L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }).setContent('Ran here!')
+    )
+    .openPopup();
+};
