f7e789dcdfc379ae3ae140fa756b5b6e32e78131
pranav-kural <pranavkural042@gmail.com>
2022-05-18 20:10:14 -0600

    refactored the code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
+// DOM elements
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,46 +11,82 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-  pos => {
-    const { latitude } = pos.coords;
-    const { longitude } = pos.coords;
-    // render leaflet interactive map
-    leafletMap(latitude, longitude);
-  },
-  () => alert('Could not get your position')
-);
+class App {
+  #map;
+  #mapEvent;
 
-const leafletMap = function (latitude, longitude) {
-  // render map
-  const map = renderMap(latitude, longitude);
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-  // map click event handler
-  map.on('click', mapEvent => mapClickHandler(mapEvent, map));
-};
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+      alert('Could not get your position')
+    );
+  }
 
-const renderMap = (latitude, longitude) => {
-  const map = L.map('map').setView([latitude, longitude], 16);
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+    this.#map = L.map('map').setView([latitude, longitude], 16);
 
-  return map;
-};
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-const mapClickHandler = (mapEvent, map) => {
-  L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      }).setContent('Ran here!')
-    )
-    .openPopup();
-};
+    // map click event handler
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(event) {
+    // display the form for entering details about activity at the location clicked at
+    form.classList.remove('hidden');
+    inputDistance.focus();
+    // update global state
+    console.log(event);
+    this.#mapEvent = event;
+    // display empty marker
+    L.marker([this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng])
+      .addTo(this.#map)
+      .bindPopup('Enter activity details')
+      .openPopup();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    // prevent page reload on submit
+    e.preventDefault();
+
+    // display marker
+    L.marker([this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }).setContent(`Ran ${inputDistance.value} here!`)
+      )
+      .openPopup();
+
+    // reset initial values
+    // prettier-ignore
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    form.classList.add('hidden');
+  }
+}
+
+if (navigator.geolocation) {
+  const app = new App();
+} else alert('Geolocation API not available!');
